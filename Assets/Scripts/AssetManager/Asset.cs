c3c24e38bd2084523bd82548d22559b60640d0c8
TychoZaal <TychoZaal@gmail.com>
2024-01-27 15:07:28 +0100

    ASSET MANAGER COOL NU

--- Assets/Scripts/AssetManager/Asset.cs
+++ Assets/Scripts/AssetManager/Asset.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Asset : MonoBehaviour
+public class Asset
 {
     public enum Category { FACE, HAIR, EYEBROW, EYE, NOSE, MOUTH, BEARD }
 
