8f9a69901302fa91ce14b7549f1e87097c36c423
A.E. Özbay <ozbay.ae@gmail.com>
2024-01-28 13:53:55 +0100

    crane animation depends on seconds passed

--- Assets/Lisa's Folder/MoveHookAlongRail.cs
+++ Assets/Lisa's Folder/MoveHookAlongRail.cs
@@ -37,17 +37,23 @@ public class MoveHookAlongRail : MonoBehaviour
     public void TriggerMoveAway(GameObject hook)
     {
         LTSeq sequence = LeanTween.sequence();
-
-        sequence.append(LeanTween.move(hook, movepoints[0].transform.position, 2.0f).setEase(LeanTweenType.easeInOutQuad));
+        //time passed since start of game
+        float timePassed = Time.timeSinceLevelLoad;
+        float timeToSpawn = 3.0f - (timePassed / 40f);
+        sequence.append(LeanTween.move(hook, movepoints[0].transform.position, timeToSpawn).setEase(LeanTweenType.easeInOutQuad));
         sequence.append(LeanTween.move(hook, movepoints[1].transform.position, 0.1f));
-        sequence.append(LeanTween.move(hook, movepoints[2].transform.position, 2.0f).setEase(LeanTweenType.easeInOutQuad));
+        sequence.append(LeanTween.move(hook, movepoints[2].transform.position, timeToSpawn).setEase(LeanTweenType.easeInOutQuad));
         sequence.append(LeanTween.move(hook, movepoints[4].transform.position, 0.1f));
-        sequence.append(LeanTween.move(hook, movepoints[3].transform.position, 2.0f).setEase(LeanTweenType.easeInOutQuad));
+        sequence.append(LeanTween.move(hook, movepoints[3].transform.position, timeToSpawn).setEase(LeanTweenType.easeInOutQuad));
     }
 
     public void TriggerSpawnAnimation(GameObject hook)
     {
-        LeanTween.move(hook, spawnPosition.transform.position, 1.0f).setEase(LeanTweenType.easeInOutQuad);
+        //time passed since start of game
+        float timePassed = Time.timeSinceLevelLoad;
+        //decrease the time to spawn the hook by 0.1 seconds for every 10 seconds passed
+        float timeToSpawn = 2.0f - (timePassed / 40f);
+        LeanTween.move(hook, spawnPosition.transform.position, timeToSpawn).setEase(LeanTweenType.easeInOutQuad);
     }
 
 }
